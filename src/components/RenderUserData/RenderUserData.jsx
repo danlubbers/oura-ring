28a5389b773d9d9a5aacd02f8c1bd20f22400365
Dan Lubbers <lubbers.dan@gmail.com>
2021-11-16 19:07:16 -0500

    Moved all logic to parent component userData

--- src/components/RenderUserData/RenderUserData.jsx
+++ src/components/RenderUserData/RenderUserData.jsx
@@ -1,15 +1,54 @@
 import * as styles from "./RenderUserData.module.scss";
 import Container from "../Container/Container";
+import Button from "../Button/Button";
 
-const RenderUserData = ({ userData }) => {
+const RenderUserData = ({
+  age,
+  height,
+  weight,
+  gender,
+  email,
+  isImperial,
+  setUnits,
+}) => {
   return (
     <Container>
       <div className={styles.renderUserDataContainer}>
-        <p>AGE: {userData?.age}</p>
-        <p>HEIGHT: {userData?.height}</p>
-        <p>WEIGHT: {userData?.weight}</p>
-        <p>GENDER: {userData?.gender}</p>
-        <p>EMAIL: {userData?.email}</p>
+        <div>
+          <span>AGE:</span>
+          <span>{age}</span>
+        </div>
+        <div>
+          <span>HEIGHT:</span>
+          <span>{height}</span>
+          {isImperial ? <span>ft</span> : <span>cm</span>}
+        </div>
+        <div>
+          <span>WEIGHT:</span>
+          <span>{weight}</span>
+          {isImperial ? <span>lbs</span> : <span>kg</span>}
+        </div>
+        <div>
+          <span>GENDER:</span>
+          <span>{gender}</span>
+        </div>
+        <div>
+          <span>EMAIL:</span>
+          <span>{email}</span>
+        </div>
+      </div>
+      <div className={styles.unitsBtnWrapper}>
+        <Button
+          btnAction="Imperial"
+          type="text"
+          onClick={() => setUnits("imperial")}
+        />
+
+        <Button
+          btnAction="Metric"
+          type="text"
+          onClick={() => setUnits("metric")}
+        />
       </div>
     </Container>
   );
--- src/pages/userData.jsx
+++ src/pages/userData.jsx
@@ -1,13 +1,41 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import RenderUserData from "../components/RenderUserData/RenderUserData";
 import { GlobalContext } from "../context/Provider";
 
 function UserData() {
+  const [isImperial, setIsImperial] = useState(false);
   const { ouraData } = useContext(GlobalContext);
   const userData = ouraData?.ouraUserData?.data;
+
+  const age = userData?.age;
+  const height = isImperial
+    ? Math.round((userData?.height + Number.EPSILON) * 0.0328084 * 100) / 100
+    : userData?.height;
+  const weight = isImperial
+    ? Math.round(userData?.weight * 2.2046)
+    : userData?.weight;
+  const gender = userData?.gender;
+  const email = userData?.email;
+
+  const setUnits = (unit) => {
+    if (unit === "imperial") {
+      setIsImperial(true);
+    } else {
+      setIsImperial(false);
+    }
+  };
+
   return (
     <div>
-      <RenderUserData userData={userData} />
+      <RenderUserData
+        age={age}
+        height={height}
+        weight={weight}
+        gender={gender}
+        email={email}
+        isImperial={isImperial}
+        setUnits={setUnits}
+      />
     </div>
   );
 }
