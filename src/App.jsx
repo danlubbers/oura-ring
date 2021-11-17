ca54419e8bc9205117490f1bbfe26dfc5109c844
Dan Lubbers <lubbers.dan@gmail.com>
2021-11-16 19:02:36 -0500

    Added a useToken hook and changed the token to save to localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
 import "./App.scss";
 import routes from "./routes";
 import Login from "./pages/login";
+import useToken from "./hooks/useToken";
 
 function App() {
-  const [token, setToken] = useState(null);
+  const { token, setToken } = useToken();
+
   console.log(`token`, token);
 
   if (!token) {
