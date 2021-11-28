996e71a89b7aa9b6576524cd305a2f5c76c88e98
Dan Lubbers <lubbers.dan@gmail.com>
2021-11-28 15:10:30 -0500

    Setting up logic for user picking which day from the past week to show data

--- src/pages/sleepData.jsx
+++ src/pages/sleepData.jsx
@@ -1,12 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
+
 import NavigationHeader from "../components/NavigationHeader/NavigationHeader";
+import Button from "../components/Button/Button";
 import RenderSleepData from "../components/RenderSleepData/RenderSleepData";
 import { GlobalContext } from "../context/Provider";
 
 function SleepData() {
   const { sleepData } = useContext(GlobalContext);
-  // console.log(`sleepData`, sleepData);
-  const prevNightsData = sleepData?.[sleepData?.length - 1];
+  console.log(`sleepData`, sleepData);
+  const [date, setDate] = useState(null);
+
+  useEffect(() => {
+    const todaysDate = sleepData?.[sleepData.length - 1]?.summary_date.slice(5);
+    setDate(todaysDate);
+  }, []);
+
+  // const [weekDays, setWeekDays] = useState([]);
+
+  const pickSleepDate = sleepData.map((date, idx) => {
+    // console.log(`date`, date.summary_date);
+    const day = date.summary_date.slice(5);
+    console.log(`day`, day);
+
+    return (
+      <div key={`btn ${day}`} style={{ width: "100%" }}>
+        <Button btnAction={day} onClick={() => setDate(day)} />
+      </div>
+    );
+  });
+
+  // console.log(`weekDays`, weekDays);
+
+  console.log(`date`, date);
+
+  const prevNightsData = sleepData?.[sleepData.length - 1];
   // console.log(`prevNightsData`, prevNightsData);
   const summaryDate = prevNightsData?.summary_date;
 
@@ -47,10 +74,22 @@ function SleepData() {
   return (
     <div>
       <NavigationHeader />
+
+      <div
+        style={{
+          width: "100%",
+          marginTop: "2rem",
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        {pickSleepDate}
+      </div>
+
       <RenderSleepData
         bedtimeStart={bedtimeStart}
         bedtimeEnd={bedtimeEnd}
-        summaryDate={summaryDate}
+        summaryDate={date}
         avgHRData={avgHRData}
         data={prevNightsData}
         width={"width"}
