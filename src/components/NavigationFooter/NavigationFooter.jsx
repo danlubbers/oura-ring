b6ff5e0344762a96f9d99b3ddbbf534aa30d7a84
Dan Lubbers <lubbers.dan@gmail.com>
2021-11-29 16:41:18 -0500

    Navigation Footer Icon is now highlighted depending on which screen is active

--- src/components/NavigationFooter/NavigationFooter.jsx
+++ src/components/NavigationFooter/NavigationFooter.jsx
@@ -1,25 +1,36 @@
 import React from "react";
 import * as styles from "./NavigationFooter.module.scss";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { AiFillHome } from "react-icons/ai";
 import { FaUser } from "react-icons/fa";
 import { GiNightSleep } from "react-icons/gi";
 import { RiHealthBookFill } from "react-icons/ri";
 
 const NavigationHeader = () => {
+  const { pathname } = useLocation();
+
   return (
     <div className={styles.navigationContainer}>
       <Link className={styles.link} to="/">
-        <AiFillHome size={35} />
+        <AiFillHome size={35} color={pathname === "/" ? "#66becc" : null} />
       </Link>
       <Link className={styles.link} to="/user-data">
-        <FaUser size={35} />
+        <FaUser
+          size={35}
+          color={pathname === "/user-data" ? "#66becc" : null}
+        />
       </Link>
       <Link className={styles.link} to="/readiness-data">
-        <RiHealthBookFill size={35} />
+        <RiHealthBookFill
+          size={35}
+          color={pathname === "/readiness-data" ? "#66becc" : null}
+        />
       </Link>
       <Link className={styles.link} to="/sleep-data">
-        <GiNightSleep size={35} />
+        <GiNightSleep
+          size={35}
+          color={pathname === "/sleep-data" ? "#66becc" : null}
+        />
       </Link>
     </div>
   );
