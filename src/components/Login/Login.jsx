b68a6e97c3fc12bd749355ccd8ce55c4ca861164
Dan Lubbers <lubbers.dan@gmail.com>
2021-12-13 16:11:59 -0500

    Added basic error handling for username and password

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,14 +4,24 @@ import logo from "../../assets/logo/default_seo_image.png";
 import Input from "../Input/Input";
 import Button from "../Button/Button";
 
-const Login = ({ handleUsername, handlePassword, handleSubmit }) => {
+const Login = ({ handleUsername, handlePassword, handleSubmit, error }) => {
+  console.log(`error`, error);
+  const { usernameError, passwordError } = error;
+
   return (
     <div className={styles.loginContainer}>
       <img className={styles.logo} src={logo} alt="logo" />
       <form className={styles.formWrapper} onSubmit={handleSubmit}>
         <p className={styles.usernameText}>Enter Username</p>
+        {usernameError && (
+          <p className={styles.usernameErrorText}>{usernameError}</p>
+        )}
+
         <Input type="text" placeholder="username" onChange={handleUsername} />
         <p className={styles.passwordText}>Enter Password </p>
+        {passwordError && (
+          <p className={styles.passwordErrorText}>{passwordError}</p>
+        )}
         <Input
           type="password"
           placeholder="password"
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -5,6 +5,10 @@ import { loginUser } from "../utilities/loginUser";
 const Login = ({ setToken }) => {
   const [username, setUsername] = useState(null);
   const [password, setPassword] = useState(null);
+  const [error, setError] = useState({
+    usernameError: null,
+    passwordError: null,
+  });
 
   const handleUsername = (e) => {
     setUsername(e.target.value);
@@ -26,6 +30,23 @@ const Login = ({ setToken }) => {
       });
       await setToken(token);
     }
+    if (
+      username !== process.env.REACT_APP_USERNAME &&
+      password !== process.env.REACT_APP_PASSWORD
+    ) {
+      setError({
+        usernameError: "The username you have submitted is incorrect!",
+        passwordError: "The password you have submitted is incorrect!",
+      });
+    } else if (username !== process.env.REACT_APP_USERNAME) {
+      setError({
+        usernameError: "The username you have submitted is incorrect!",
+      });
+    } else if (password !== process.env.REACT_APP_PASSWORD) {
+      setError({
+        passwordError: "The password you have submitted is incorrect!",
+      });
+    }
   };
 
   console.log(`username`, username);
@@ -36,6 +57,7 @@ const Login = ({ setToken }) => {
         handleUsername={handleUsername}
         handlePassword={handlePassword}
         handleSubmit={handleSubmit}
+        error={error}
       />
     </>
   );
