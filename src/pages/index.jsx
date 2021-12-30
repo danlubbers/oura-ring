5b498ea4a4fbb7b4878762dd2204c2a4f15bc613
Dan Lubbers <lubbers.dan@gmail.com>
2021-12-29 22:26:33 -0500

    Using exported CSV test data and looping over to get average bedroom temperature

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,45 @@
+import { useEffect, useState } from "react";
 import Home from "../components/Home/Home";
 import useToken from "../hooks/useToken";
 import NavigationHeader from "../components/NavigationFooter/NavigationFooter";
+import Papa from "papaparse";
+import { thermoStr } from "../utilities/sampleTempData";
 
 function Index() {
   const { logout } = useToken();
+  const [parsedCsvData, setParsedCsvData] = useState([]);
+  const [bedroomTemp, setBedroomTemp] = useState(null);
 
+  useEffect(() => {
+    const parseFile = (file) => {
+      Papa.parse(file, {
+        header: true,
+        complete: (results) => {
+          setParsedCsvData(results.data);
+        },
+      });
+    };
+    parseFile(thermoStr);
+
+    if (parsedCsvData.length) {
+      const tempAvg =
+        parsedCsvData
+          .map((obj) => {
+            return Object.keys(obj)
+              .filter((key) => key.includes("Temperature_Fahrenheit"))
+              .reduce(
+                (cur, key) => Object.assign(cur, { [key]: obj[key] }),
+                {}
+              );
+          })
+          .reduce((acc, curr) => acc + Number(curr.Temperature_Fahrenheit), 0) /
+        parsedCsvData.length;
+
+      setBedroomTemp(Number(tempAvg.toFixed(2)));
+    }
+  }, []);
+  // console.log(`parsedCsvData`, parsedCsvData);
+  console.log(`bedroomTemp`, bedroomTemp);
   return (
     <div>
       <Home logout={logout} />
