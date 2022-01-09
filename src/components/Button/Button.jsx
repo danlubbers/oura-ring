828dd560098db8e414f5d8a5116a929f77782b77
Dan Lubbers <lubbers.dan@gmail.com>
2022-01-09 16:47:37 -0500

    Using ref in DateRenderer so when the user navigates between screens on the NavigationFooter the date selected stays in view. Right now the most recent date upon load does not load which needs to be fixed next.

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import * as styles from "./Button.module.scss";
 
 const Button = ({ btnAction, btnType, onClick, style }) => {
+  const elementRef = useRef();
+
+  useEffect(() => {
+    const btnRef = elementRef.current;
+    console.log(btnRef); // logs <div>I'm an element</div>
+  }, []);
   return (
-    <div className={styles.buttonWrapper}>
+    <div className={styles.buttonWrapper} ref={elementRef}>
       <button
         type={btnType}
         style={style}
