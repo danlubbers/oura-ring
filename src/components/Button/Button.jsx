2edd947c15fc6d14da20d67b643e83b56048649f
Dan Lubbers <lubbers.dan@gmail.com>
2022-01-09 19:43:26 -0500

    On page load the DateRenderer goes to the most recent date, but now a bug has arisen that when the user clicks a previous date that is off the screen, it will jump back to the most recent date. Saving at this point before I go on vacation.

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,9 @@
-import React, { useRef, useEffect } from "react";
+// import React, { useRef, useEffect } from "react";
 import * as styles from "./Button.module.scss";
 
 const Button = ({ btnAction, btnType, onClick, style }) => {
-  const elementRef = useRef();
-
-  useEffect(() => {
-    const btnRef = elementRef.current;
-    console.log(btnRef); // logs <div>I'm an element</div>
-  }, []);
   return (
-    <div className={styles.buttonWrapper} ref={elementRef}>
+    <div className={styles.buttonWrapper}>
       <button
         type={btnType}
         style={style}
--- src/components/DateRenderer/DateRenderer.jsx
+++ src/components/DateRenderer/DateRenderer.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useCallback, useRef, useEffect } from "react";
 import * as styles from "./DateRenderer.module.scss";
 import Button from "../Button/Button";
 import { GlobalContext } from "../../context/Provider";
@@ -9,6 +9,12 @@ const DateRenderer = () => {
 
   const todaysDate = todaysData?.date;
 
+  const scrollRef = useRef();
+
+  useEffect(() => {
+    scrollRef?.current?.scrollIntoView();
+  }, [todaysDate]); // this triggers to go to most recent date on load, but creates another bug when user selects different date. Right now it always scrolls no matter what to the end. Need to target the specific date instead.
+
   const pickSleepDate = sleepData.map((sleepObj, idx) => {
     const combinedData = {
       readiness: readinessData[idx],
@@ -18,8 +24,17 @@ const DateRenderer = () => {
 
     const date = sleepObj.bedtime_end.slice(5, 10);
 
+    // const scrollDate = scrollRef?.current?.outerText;
+    // console.log(`scrollDate`, scrollDate);
+    // const scrollWidth = scrollRef?.current?.offsetLeft;
+    // console.log(`scrollWidth`, scrollWidth);
+    // window.scrollTo({
+    //   left: scrollWidth,
+    //   behavior: "smooth",
+    // });
+
     return (
-      <div key={`btn: ${date}`} style={{ width: "100%" }}>
+      <div key={`btn: ${date}`} style={{ width: "100%" }} ref={scrollRef}>
         <Button
           btnAction={date}
           onClick={() =>
