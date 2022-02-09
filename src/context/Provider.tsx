ca9be1b3bb3a26fefbbe530308ffe1e4d635e508
Dan Lubbers <lubbers.dan@gmail.com>
2022-02-09 17:01:01 -0500

    The Context Provider his now converted to typescript.

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -1,28 +1,75 @@
 import { createContext, useState, useEffect } from "react";
 import getOuraData from "../utilities/getOuraData";
 
-export const GlobalContext = createContext({});
+interface GlobalContextProps {
+  userData: {};
+  readinessData: any;
+  sleepData: any;
+  activityData: any;
+  startDate: string;
+  setStartDate: (date: string) => void;
+  endDate: string;
+  setEndDate: (date: string) => void;
+  todaysData: {};
+  setTodaysData: (
+    date: string,
+    bedtimeStart: string,
+    bedtimeEnd: string,
 
-const GlobalProvider = ({ children }) => {
+    data: {
+      readiness: {};
+      sleep: {};
+      activity: {};
+    }
+  ) => void;
+  isMobileDisplay: boolean;
+  setIsMobileDisplay: (isMobile: boolean) => void;
+  btnOffsetLeft: boolean;
+  setBtnOffsetLeft: (offset: boolean) => void;
+  isBtnPosition: boolean;
+  setIsBtnPosition: (isBtnPosition: boolean) => void;
+}
+
+export const GlobalContext = createContext<GlobalContextProps>({
+  userData: {},
+  readinessData: [],
+  sleepData: [],
+  activityData: [],
+  startDate: "",
+  setStartDate: () => "",
+  endDate: "",
+  setEndDate: () => "",
+  todaysData: {},
+  setTodaysData: () => {},
+  isMobileDisplay: false,
+  setIsMobileDisplay: () => false,
+  btnOffsetLeft: false,
+  setBtnOffsetLeft: () => false,
+  isBtnPosition: true,
+  setIsBtnPosition: () => true,
+});
+
+// Figure out the fix for TS children so it's not set to "any"
+const GlobalProvider = ({ children }: any) => {
   const [userData, setUserData] = useState([]);
   const [readinessData, setReadinessData] = useState([]);
   const [sleepData, setSleepData] = useState([]);
   const [activityData, setActivityData] = useState([]);
-  const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
   const [todaysData, setTodaysData] = useState({});
   const [isMobileDisplay, setIsMobileDisplay] = useState(false);
-  const [btnOffsetLeft, setBtnOffsetLeft] = useState();
+  const [btnOffsetLeft, setBtnOffsetLeft] = useState(false);
   const [isBtnPosition, setIsBtnPosition] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await getOuraData();
 
-      const userData = data.ouraUserData.data;
-      const readinessData = data.ouraReadinessData.data.readiness;
-      const sleepData = data.ouraSleepData.data.sleep;
-      const activityData = data.ouraActivityData.data.activity;
+      const userData = data?.ouraUserData.data;
+      const readinessData = data?.ouraReadinessData.data.readiness;
+      const sleepData = data?.ouraSleepData.data.sleep;
+      const activityData = data?.ouraActivityData.data.activity;
 
       const startDate = String(new Date(sleepData[0].bedtime_end)).slice(0, 15);
       const endDate = String(
