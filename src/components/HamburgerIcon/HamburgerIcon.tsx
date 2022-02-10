056720f18445b519f9426d886f474086f52f8c17
Dan Lubbers <lubbers.dan@gmail.com>
2022-02-10 15:07:18 -0500

    HamburgerIcon has been converted to typescript.

--- src/components/HamburgerIcon/HamburgerIcon.tsx
+++ src/components/HamburgerIcon/HamburgerIcon.tsx
@@ -1,7 +1,12 @@
-import React from "react";
 import styles from "./HamburgerIcon.module.scss";
 
-const HamburgerIcon = ({ handleClickMobileDisplay, isMobileDisplay }) => {
+const HamburgerIcon = ({
+  handleClickMobileDisplay,
+  isMobileDisplay,
+}: {
+  handleClickMobileDisplay: () => void;
+  isMobileDisplay: boolean;
+}) => {
   return (
     <nav className={styles.menubarWrapper}>
       <div
