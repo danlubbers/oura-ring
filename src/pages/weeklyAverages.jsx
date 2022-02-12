d15106146192028ab6a7d6f76fa4210472b58b59
Dan Lubbers <lubbers.dan@gmail.com>
2022-02-12 14:18:32 -0500

    PickDateRange has been converted to typescript.

--- src/pages/weeklyAverages.jsx
+++ src/pages/weeklyAverages.jsx
@@ -72,7 +72,7 @@ const WeeklyAverages = () => {
 
   // console.log(`parsedCsvData`, parsedCsvData);
 
-  const weeklyAverages = sleepData.map((obj, idx) => {
+  const weeklyAverages = sleepData.map((obj) => {
     const date = String(new Date(obj.bedtime_end)).slice(0, 15);
 
     const monthDayDate = obj.bedtime_end.slice(5, 10);
