277b3f829e8d760643e03aa30f501fa83375172a
Dan Lubbers <lubbers.dan@gmail.com>
2022-02-27 16:40:29 -0500

    Moved types in Provider to dataTypes file. Created an initialState file and moved the initial state objects to that file to clean up the code in the Provider.

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -1,26 +1,16 @@
 import { FC, createContext, useState, useEffect } from "react";
 import getOuraData from "../utilities/getOuraData";
-import { ReadinessProps, SleepProps, ActivityProps } from "../types/dataTypes";
-
-type UserProps = {
-  age: number;
-  email: string;
-  gender: string;
-  height: number;
-  weight: number;
-};
-
-type TodaysProps = {
-  date: string;
-  bedtimeStart: string;
-  bedtimeEnd: string;
-
-  data: {
-    readiness: ReadinessProps;
-    sleep: SleepProps;
-    activity: ActivityProps;
-  };
-};
+import {
+  UserProps,
+  TodaysProps,
+  ReadinessProps,
+  SleepProps,
+  ActivityProps,
+} from "../types/dataTypes";
+import {
+  userInitialStateData,
+  todaysInitialStateData,
+} from "../initialState/initialState";
 
 interface GlobalContextProps {
   userData: UserProps;
@@ -42,13 +32,7 @@ interface GlobalContextProps {
 }
 
 export const GlobalContext = createContext<GlobalContextProps>({
-  userData: {
-    age: 0,
-    email: "",
-    gender: "",
-    height: 0,
-    weight: 0,
-  },
+  userData: userInitialStateData,
   readinessData: [],
   sleepData: [],
   activityData: [],
@@ -56,104 +40,7 @@ export const GlobalContext = createContext<GlobalContextProps>({
   setStartDate: () => "",
   endDate: "",
   setEndDate: () => "",
-  todaysData: {
-    date: "",
-    bedtimeStart: "",
-    bedtimeEnd: "",
-    data: {
-      readiness: {
-        period_id: 0,
-        rest_mode_state: 0,
-        score: 0,
-        score_activity_balance: 0,
-        score_hrv_balance: 0,
-        score_previous_day: 0,
-        score_previous_night: 0,
-        score_recovery_index: 0,
-        score_resting_hr: 0,
-        score_sleep_balance: 0,
-        score_temperature: 0,
-        summary_date: "",
-      },
-      sleep: {
-        awake: 0,
-        bedtime_end: "",
-        bedtime_end_delta: 0,
-        bedtime_start: "",
-        bedtime_start_delta: 0,
-        breath_average: 0,
-        deep: 0,
-        duration: 0,
-        efficiency: 0,
-        hr_5min: [],
-        hr_average: 0,
-        hr_lowest: 0,
-        hypnogram_5min: "",
-        is_longest: 0,
-        light: 0,
-        midpoint_at_delta: 0,
-        midpoint_time: 0,
-        onset_latency: 0,
-        period_id: 0,
-        rem: 0,
-        restless: 0,
-        rmssd: 0,
-        rmssd_5min: [],
-        score: 0,
-        score_alignment: 0,
-        score_deep: 0,
-        score_disturbances: 0,
-        score_efficiency: 0,
-        score_latency: 0,
-        score_rem: 0,
-        score_total: 0,
-        summary_date: "",
-        temperature_delta: 0,
-        temperature_deviation: 0,
-        temperature_trend_deviation: 0,
-        timezone: 0,
-        total: 0,
-      },
-      activity: {
-        average_met: 0,
-        cal_active: 0,
-        cal_total: 0,
-        class_5min: "",
-        daily_movement: 0,
-        day_end: "",
-        day_start: "",
-        high: 0,
-        inactive: 0,
-        inactivity_alerts: 0,
-        low: 0,
-        medium: 0,
-        met_1min: [],
-        met_min_high: 0,
-        met_min_inactive: 0,
-        met_min_low: 0,
-        met_min_medium: 0,
-        non_wear: 0,
-        rest: 0,
-        rest_mode_state: 0,
-        score: 0,
-        score_meet_daily_targets: 0,
-        score_move_every_hour: 0,
-        score_recovery_time: 0,
-        score_stay_active: 0,
-        score_training_frequency: 0,
-        score_training_volume: 0,
-        steps: 0,
-        summary_date: "",
-        target_calories: 0,
-        target_km: 0,
-        target_miles: 0,
-        timezone: 0,
-        to_target_km: 0,
-        to_target_miles: 0,
-        total: 0,
-      },
-    },
-  },
+  todaysData: todaysInitialStateData,
   setTodaysData: () => {},
   isMobileDisplay: false,
   setIsMobileDisplay: () => false,
@@ -164,116 +51,15 @@ export const GlobalContext = createContext<GlobalContextProps>({
 });
 
 const GlobalProvider: FC = ({ children }) => {
-  const [userData, setUserData] = useState<UserProps>({
-    age: 0,
-    email: "",
-    gender: "",
-    height: 0,
-    weight: 0,
-  });
+  const [userData, setUserData] = useState<UserProps>(userInitialStateData);
   const [readinessData, setReadinessData] = useState<ReadinessProps[]>([]);
   const [sleepData, setSleepData] = useState<SleepProps[]>([]);
   const [activityData, setActivityData] = useState<ActivityProps[]>([]);
   const [startDate, setStartDate] = useState<string>("");
   const [endDate, setEndDate] = useState<string>("");
-  const [todaysData, setTodaysData] = useState<TodaysProps>({
-    date: "",
-    bedtimeStart: "",
-    bedtimeEnd: "",
-    data: {
-      readiness: {
-        period_id: 0,
-        rest_mode_state: 0,
-        score: 0,
-        score_activity_balance: 0,
-        score_hrv_balance: 0,
-        score_previous_day: 0,
-        score_previous_night: 0,
-        score_recovery_index: 0,
-        score_resting_hr: 0,
-        score_sleep_balance: 0,
-        score_temperature: 0,
-        summary_date: "",
-      },
-      sleep: {
-        awake: 0,
-        bedtime_end: "",
-        bedtime_end_delta: 0,
-        bedtime_start: "",
-        bedtime_start_delta: 0,
-        breath_average: 0,
-        deep: 0,
-        duration: 0,
-        efficiency: 0,
-        hr_5min: [],
-        hr_average: 0,
-        hr_lowest: 0,
-        hypnogram_5min: "",
-        is_longest: 0,
-        light: 0,
-        midpoint_at_delta: 0,
-        midpoint_time: 0,
-        onset_latency: 0,
-        period_id: 0,
-        rem: 0,
-        restless: 0,
-        rmssd: 0,
-        rmssd_5min: [],
-        score: 0,
-        score_alignment: 0,
-        score_deep: 0,
-        score_disturbances: 0,
-        score_efficiency: 0,
-        score_latency: 0,
-        score_rem: 0,
-        score_total: 0,
-        summary_date: "",
-        temperature_delta: 0,
-        temperature_deviation: 0,
-        temperature_trend_deviation: 0,
-        timezone: 0,
-        total: 0,
-      },
-      activity: {
-        average_met: 0,
-        cal_active: 0,
-        cal_total: 0,
-        class_5min: "",
-        daily_movement: 0,
-        day_end: "",
-        day_start: "",
-        high: 0,
-        inactive: 0,
-        inactivity_alerts: 0,
-        low: 0,
-        medium: 0,
-        met_1min: [],
-        met_min_high: 0,
-        met_min_inactive: 0,
-        met_min_low: 0,
-        met_min_medium: 0,
-        non_wear: 0,
-        rest: 0,
-        rest_mode_state: 0,
-        score: 0,
-        score_meet_daily_targets: 0,
-        score_move_every_hour: 0,
-        score_recovery_time: 0,
-        score_stay_active: 0,
-        score_training_frequency: 0,
-        score_training_volume: 0,
-        steps: 0,
-        summary_date: "",
-        target_calories: 0,
-        target_km: 0,
-        target_miles: 0,
-        timezone: 0,
-        to_target_km: 0,
-        to_target_miles: 0,
-        total: 0,
-      },
-    },
-  });
+  const [todaysData, setTodaysData] = useState<TodaysProps>(
+    todaysInitialStateData
+  );
   const [isMobileDisplay, setIsMobileDisplay] = useState<boolean>(false);
   const [btnOffsetLeft, setBtnOffsetLeft] = useState<number>(0);
   const [isBtnPosition, setIsBtnPosition] = useState<boolean>(true);
