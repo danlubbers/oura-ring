de7160923735dde9fe767def0da52a1637c08d8e
Dan Lubbers <lubbers.dan@gmail.com>
2022-04-04 14:29:37 -0400

    Refactored login test a bit

--- src/pages/login.test.jsx
+++ src/pages/login.test.jsx
@@ -1,45 +1,45 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { BrowserRouter as Router } from "react-router-dom";
 import Login from "./login";
 
-let renderScreen;
-
 beforeEach(() => {
-  renderScreen = render(
+  render(
     <Router>
       <Login />
     </Router>
   );
 });
 
-test("Enter Username & Password are displayed correctly", () => {
-  const enterUsernameElement = renderScreen.getByText(/enter username/i);
-  expect(enterUsernameElement.textContent).toBe("Enter Username");
+describe("login", () => {
+  test("Enter Username & Password are displayed correctly", () => {
+    const enterUsernameElement = screen.getByText(/enter username/i);
+    expect(enterUsernameElement.textContent).toBe("Enter Username");
 
-  const enterPasswordElement = renderScreen.getByText(/Enter Password/i);
-  expect(enterPasswordElement.textContent).toBe("Enter Password");
-});
-
-test("Inputs should be blank on initial render", () => {
-  const usernameInputElement = renderScreen.getByRole("textbox");
-  expect(usernameInputElement.value).toBe("");
-  const passwordInputElement = renderScreen.getByLabelText(/password/i);
-  expect(passwordInputElement.value).toBe("");
-});
-
-test("Username and Password are entered correctly", () => {
-  const username = "dlubbers";
-  const password = "passtest";
-
-  const usernameInputElement = renderScreen.getByRole("textbox", {
-    name: /username/i,
+    const enterPasswordElement = screen.getByText(/Enter Password/i);
+    expect(enterPasswordElement.textContent).toBe("Enter Password");
   });
-  const passwordInputElement = renderScreen.getByLabelText("Enter Password");
 
-  userEvent.type(usernameInputElement, "dlubbers");
-  userEvent.type(passwordInputElement, "passtest");
+  test("Inputs should be blank on initial render", () => {
+    const usernameInputElement = screen.getByRole("textbox");
+    expect(usernameInputElement.value).toBe("");
+    const passwordInputElement = screen.getByLabelText(/password/i);
+    expect(passwordInputElement.value).toBe("");
+  });
 
-  expect(usernameInputElement.value).toBe(username);
-  expect(passwordInputElement.value).toBe(password);
+  test("Username and Password are entered correctly", () => {
+    const username = "dlubbers";
+    const password = "passtest";
+
+    const usernameInputElement = screen.getByRole("textbox", {
+      name: /username/i,
+    });
+    const passwordInputElement = screen.getByLabelText("Enter Password");
+
+    userEvent.type(usernameInputElement, "dlubbers");
+    userEvent.type(passwordInputElement, "passtest");
+
+    expect(usernameInputElement.value).toBe(username);
+    expect(passwordInputElement.value).toBe(password);
+  });
 });
