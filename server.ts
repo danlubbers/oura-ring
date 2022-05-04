8834bff0b8744a81c42b4c6f58e23685a2428b4c
Dan Lubbers <lubbers.dan@gmail.com>
2022-05-04 12:51:45 -0400

    Server now console.logs tag data for V2 API

--- server.ts
+++ server.ts
@@ -1,6 +1,29 @@
+require("dotenv").config();
+const axios = require("axios");
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
+const ouraToken = process.env.REACT_APP_OURA_TOKEN;
+
+const baseURL = `https://api.ouraring.com/v2/usercollection/`;
+
+const headerConfig = {
+  Accept: "application/json",
+  "Content-Type": "application/json",
+  Authorization: `Bearer ${ouraToken}`,
+};
+
+// V2 Endpoints
+const ouraTagsV2BaseURL = `${baseURL}tag?start_date=2021-12-24`;
+
+const ouraTagsData = axios
+  .get(ouraTagsV2BaseURL, {
+    headers: headerConfig,
+  })
+  .then((res) => console.log(res.data));
+
+// console.log("ouraTagsData", ouraTagsData);
 
 app.use(cors());
 
--- src/utilities/getOuraData.ts
+++ src/utilities/getOuraData.ts
@@ -2,17 +2,47 @@ import axios from "axios";
 
 const getOuraData = async () => {
   const ouraToken = process.env.REACT_APP_OURA_TOKEN;
-  const ouraUserInfoAPI = `https://api.ouraring.com/v1/userinfo?access_token=${ouraToken}`;
-  const ouraReadinessAPI = `https://api.ouraring.com/v1/readiness?start=2021-12-24&access_token=${ouraToken}`;
-  const ouraSleepAPI = `https://api.ouraring.com/v1/sleep?start=2021-12-24&access_token=${ouraToken}`;
-  const ouraActivityAPI = `https://api.ouraring.com/v1/activity?start=2021-12-24&access_token=${ouraToken}`;
-  try {
-    const ouraUserData = await axios.get(ouraUserInfoAPI);
-    const ouraReadinessData = await axios.get(ouraReadinessAPI);
-    const ouraSleepData = await axios.get(ouraSleepAPI);
-    const ouraActivityData = await axios.get(ouraActivityAPI);
 
-    return { ouraUserData, ouraReadinessData, ouraSleepData, ouraActivityData };
+  const headerConfig = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${ouraToken}`,
+  };
+
+  // V1 Endpoints
+  const ouraUserInfoBaseURL = `https://api.ouraring.com/v1/userinfo`;
+  const ouraReadinessBaseURL = `https://api.ouraring.com/v1/readiness?start=2021-12-24`;
+  const ouraSleepBaseURL = `https://api.ouraring.com/v1/sleep?start=2021-12-24`;
+  const ouraActivityBaseURL = `https://api.ouraring.com/v1/activity?start=2021-12-24`;
+
+  // V2 Endpoints
+  const ouraTagsV2BaseURL = `https://api.ouraring.com/v2/usercollection/tag?start_date=2021-12-24&end_date=2022-04-27`;
+
+  try {
+    const ouraUserData = await axios.get(ouraUserInfoBaseURL, {
+      headers: headerConfig,
+    });
+    const ouraReadinessData = await axios.get(ouraReadinessBaseURL, {
+      headers: headerConfig,
+    });
+    const ouraSleepData = await axios.get(ouraSleepBaseURL, {
+      headers: headerConfig,
+    });
+    const ouraActivityData = await axios.get(ouraActivityBaseURL, {
+      headers: headerConfig,
+    });
+    // const ouraTagsData = await axios.get(ouraTagsV2BaseURL, {
+    //   headers: headerConfig,
+    // });
+    // console.log("ouraTagsData", ouraTagsData);
+
+    return {
+      ouraUserData,
+      ouraReadinessData,
+      ouraSleepData,
+      ouraActivityData,
+      // ouraTagsData,
+    };
   } catch (error) {
     console.error(error);
   }
