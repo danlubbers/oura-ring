c6709084e3712f0ba6bcf0e35557d572951db38b
Dan Lubbers <lubbers.dan@gmail.com>
2022-05-14 13:33:43 -0400

    Added a conditional to the endPoints function to differentiate between data that needs to be destructured vs data that was only an object.

--- server/server.mjs
+++ server/server.mjs
@@ -9,7 +9,9 @@ app.use(cors());
 
 const ouraToken = process.env.REACT_APP_OURA_TOKEN;
 const port = 8080;
+// https://api.ouraring.com/v2/usercollection/personal_info
 const baseURLV2 = `https://api.ouraring.com/v2/usercollection/`;
+
 const headerConfig = {
   Accept: "application/json",
   "Content-Type": "application/json",
@@ -23,18 +25,30 @@ const ouraSessionsV2BaseURL = `${baseURLV2}session?start_date=2021-12-24`;
 const ouraTagsV2BaseURL = `${baseURLV2}tag?start_date=2021-12-24`;
 const ouraWorkoutssV2BaseURL = `${baseURLV2}workout?start_date=2021-12-24`;
 
-const getEndpoint = (route, baseURL, keyName) => {
+const getEndpoint = (route, baseURL, keyName, personalInfo) => {
   app.get(route, async (req, res) => {
     try {
-      const { data } = await axios
-        .get(baseURL, {
-          headers: headerConfig,
-        })
-        .then((res) => res.data);
-      console.log("data", data);
-      res.status(200).json({
-        [keyName]: data,
-      });
+      if (personalInfo) {
+        const data = await axios
+          .get(baseURL, {
+            headers: headerConfig,
+          })
+          .then((res) => res.data);
+        res.status(200).json({
+          [keyName]: data,
+        });
+        console.log("data", data);
+      } else {
+        const { data } = await axios
+          .get(baseURL, {
+            headers: headerConfig,
+          })
+          .then((res) => res.data);
+        res.status(200).json({
+          [keyName]: data,
+        });
+        console.log("data", data);
+      }
     } catch (error) {
       res.status(400).json({ message: `*** Error Occured ***`, error });
     }
@@ -42,7 +56,7 @@ const getEndpoint = (route, baseURL, keyName) => {
 };
 
 getEndpoint("/heartrate", ouraHeartRateV2BaseURL, "heartRate");
-getEndpoint("/personal_info", ouraPersonalInfoV2BaseURL, "personalInfo");
+getEndpoint("/personal_info", ouraPersonalInfoV2BaseURL, "personalInfo", true);
 getEndpoint("/sessions", ouraSessionsV2BaseURL, "sessions");
 getEndpoint("/tags", ouraTagsV2BaseURL, "tags");
 getEndpoint("/workouts", ouraWorkoutssV2BaseURL, "workouts");
